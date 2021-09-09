170d82c33da19ab2ca6bf1c0278bb2fe8ce1ca88
ryoseitanaka <>
2021-09-09 11:16:27 +0900

    タグ登録完成

--- js/tag.js
+++ js/tag.js
@@ -1,7 +1,9 @@
 const btns = document.querySelectorAll(".tag__genre__item");
+let count = 0;
+
 
 btns.forEach(el=>{
     el.addEventListener("click",(e)=>{
-        e.target.classList.toggle("tag__genre__item--selected")
+            e.target.classList.toggle("tag__genre__item--selected");
     })
 })
